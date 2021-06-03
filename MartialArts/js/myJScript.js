560b759c33727ce7ec69e4ceb08565852bb7dccc
Gary Kirby <gazzaspc@gmail.com>
2021-06-03 13:59:21 +0100

    Added Dingbats Martial Arts & Tidy up

---
+++ MartialArts/js/myJScript.js
@@ -0,0 +1,13 @@
+﻿////Javascript in here
+
+function resetBlocker() {
+    var myBlocker = document.getElementsByClassName("blocker");
+    var myHeader = document.getElementById("header");
+
+    for (x of myBlocker) {
+        x.style.height = myHeader.offsetHeight + "px";
+    }
+}
+
+resetBlocker(); // Need this here so it resizes straight away
+window.onresize = resetBlocker; //Need this here so it resizes when the screen size changes
