538748bd13859903bff78bd26f5f010884ee7a78
Gary Kirby <gazzaspc@gmail.com>
2021-06-11 15:12:59 +0100

    1.0.5. Classes update

--- webLearning/Scripts/myScripts.js
+++ webLearning/Scripts/myScripts.js
@@ -5,3 +5,14 @@ function showMess(param1, param2) {
   document.getElementById(param1).innerHTML = param2;
   return;
 }
+
+function emptyString(e) {
+    switch (e) {
+        case "":
+        case null:
+        case typeof (e) == "undefined":
+            return true;
+        default:
+            return false;
+    }
+}
